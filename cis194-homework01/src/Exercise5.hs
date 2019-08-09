630f469a1e33101b92ddf07c56d15cd3cdf67140
Wei Ning <macgnw@gmail.com>
2019-08-09 18:11:06 +1000

    wip

--- cis194-homework01/src/Exercise5.hs
+++ cis194-homework01/src/Exercise5.hs
@@ -1,10 +1,15 @@
 module Exercise5 (runTests) where
 
-import           HanoiLib
-
 runTests :: IO ()
-runTests = testMoveSheet
+runTests =
+  print (length (hanoi 15 "a" "b" "c"))
 
 type Peg = String
 type Move = (Peg, Peg)
--- hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
+
+hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
+hanoi 1 from to _    = [(from, to)]
+hanoi n from to temp =
+  hanoi (n - 1) from temp to ++
+  hanoi 1 from to temp ++
+  hanoi (n - 1) temp to from
