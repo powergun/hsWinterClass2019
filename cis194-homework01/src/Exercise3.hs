18ff3c4177f89151b69156552503e4ad2bb6c8bf
Wei Ning <macgnw@gmail.com>
2019-08-10 11:28:28 +1000

    code review 1

--- cis194-homework01/src/Exercise3.hs
+++ cis194-homework01/src/Exercise3.hs
@@ -1,8 +1,6 @@
 module Exercise3 ( sumDigits ) where
 
-sumDigits :: [Integer] -> Integer
-sumDigits = foldr sumTwoNums 0
+import           Exercise1 (toDigits)
 
-sumTwoNums :: Integer -> Integer -> Integer
-sumTwoNums x y | x > 9     = (x `div` 10) + sumTwoNums (x `mod` 10) y
-               | otherwise = x + y
+sumDigits :: [Integer] -> Integer
+sumDigits ns = sum (sum . toDigits <$> ns)
