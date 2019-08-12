9a84bbf7e62144cb3b6db662e40abb96a241550a
Wei Ning <macgnw@gmail.com>
2019-08-13 00:02:41 +1000

    wip

--- cis194-homework02/test/Spec.hs
+++ cis194-homework02/test/Spec.hs
@@ -1,10 +1,11 @@
 
 import           Exercise1
+import           Exercise2
 import qualified Log
 import           Test.Hspec
 
-main :: IO ()
-main = hspec $
+runSpec :: IO ()
+runSpec = hspec $ do
   describe "Exercise 1" $ do
     it "individual message: expect error level-2" $
       parseMessage "E 2 562 help help\n" `shouldBe` Log.LogMessage (Log.Error 2) 562 "help help"
@@ -20,5 +21,18 @@ main = hspec $
       logMessages <- Log.testParse parseMessages 9999 "testdata/sample.log"
       length logMessages `shouldBe` 11
     it "parse whole file: expect 5523 log messages (error.log)" $ do
-      logMessages <- Log.testParse parseMessages 5523 "testdata/error.log"
+      logMessages <- Log.testParse parseMessages 9999 "testdata/error.log"
       length logMessages `shouldBe` 5523
+
+  describe "Exercise 2" $ do
+    it "insert log messages from sample.log, expect root node" $ do
+      logMessages <- Log.testParse parseMessages 9999 "testdata/sample.log"
+      let (Log.Node _ m _) = foldr insert Log.Leaf logMessages
+      m `shouldBe` Log.LogMessage (Log.Error 99) 10 "Flange failed!"
+    it "insert log messages from error.log, expect root node" $ do
+      logMessages <- Log.testParse parseMessages 9999 "testdata/error.log"
+      let (Log.Node _ m _) = foldr insert Log.Leaf logMessages
+      m `shouldBe` Log.LogMessage Log.Info 3815 "about."
+
+main :: IO ()
+main = runSpec
