9d8ea41ca3d888ca65dacd53386b9065afa8cc33
Wei Ning <nwcgnw@gmail.com>
2019-09-03 00:22:03 +1000

    exercise 1

--- cis194-homework04/src/Exercise1.hs
+++ cis194-homework04/src/Exercise1.hs
@@ -5,6 +5,8 @@ module Exercise1
     , fun2
     ) where
 
+import           Data.Bool
+
 fun1Original :: [Integer] -> Integer
 fun1Original [] = 1
 fun1Original (x:xs)
@@ -12,12 +14,21 @@ fun1Original (x:xs)
     | otherwise = fun1Original xs
 
 fun1 :: [Integer] -> Integer
-fun1 = (foldr (*) 1) . (map (\x -> if even x then x - 2 else 1))
+fun1 = (foldr (*) 1) .
+       (map (bool <$> (const 1) <*> (subtract 2) <*> even))
+-- alternative (shorter)
+-- fun1 = (foldr (*) 1) . map (subtract 2) . (filter even)
+-- even better
+-- fun1 = product . map (flip (-) 2) . (filter even)
+-- or (squeeze out the flip)
+-- fun1 = product . map (+ (-2)) . (filter even)
 
 fun2Original :: Integer -> Integer
 fun2Original 1 = 0
 fun2Original n | even n    = n + fun2Original (n `div` 2)
                | otherwise = fun2Original (3 * n + 1)
 
-fun2 :: Integer -> Integer
-fun2 _ = 1
+fun2 n = sum $
+         filter even $
+         takeWhile (> 1) $
+         iterate (bool <$> ((+ 1) . (* 3)) <*> (flip div 2) <*> even) n
