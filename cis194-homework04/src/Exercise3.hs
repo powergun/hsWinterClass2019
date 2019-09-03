63c05dbf4e5992d64a7d1b3731e8ba190c76e312
Wei Ning <wei@canva.com>
2019-09-03 11:40:03 +1000

    exercise3

--- cis194-homework04/src/Exercise3.hs
+++ cis194-homework04/src/Exercise3.hs
@@ -1,7 +1,11 @@
 module Exercise3
   ( xor
-
+  , map'
   ) where
 
 xor :: [Bool] -> Bool
 xor bs = foldr (==) True bs
+
+map' :: (a -> b) -> [a] -> [b]
+map' _ []     = []
+map' f (n:ns) = foldl (\acc elem -> acc ++ [f elem]) [f n] ns
--- cis194-homework04/test/Checks.hs
+++ cis194-homework04/test/Checks.hs
@@ -2,6 +2,7 @@ module Checks (checks) where
 
 import           Exercise1
 import           Exercise2
+import           Exercise3
 
 import           Test.QuickCheck
 
@@ -21,6 +22,14 @@ exercise2TreeHeight ns =
       actualHeight = height . foldTree $ ns
   in logHeight == actualHeight
 
+exercise3foldAsMap :: [Integer] -> Bool
+exercise3foldAsMap ns =
+  let f :: Integer -> Integer
+      f n = n ^ 2 + 4 * n + 16
+      expected = map f ns
+      actual   = map' f ns
+  in expected == actual
+
 checks :: IO ()
 checks
  = do
@@ -30,5 +39,8 @@ checks
 
   -- exercise 2
   print $ exercise2TreeHeight [1..15]
+
+  -- exercise 3
+  quickCheck(exercise3foldAsMap :: [Integer] -> Bool)
 
 
