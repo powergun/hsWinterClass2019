33eb658c6a43d02ca98ebef0b81ac44b3c696aa7
Wei Ning <nwcgnw@gmail.com>
2019-09-24 22:22:50 +1000

    wip

---
+++ cis194-homework07/src/JoinList.hs
@@ -0,0 +1,14 @@
+module JoinList
+  ( JoinList(..)
+  , (+++)
+  ) where
+
+data JoinList m a = Empty
+                  | Single m a
+                  | Append m (JoinList m a) (JoinList m a)
+                  deriving (Eq, Show)
+
+(+++) :: Monoid m => JoinList m a -> JoinList m a -> JoinList m a
+
+
+
