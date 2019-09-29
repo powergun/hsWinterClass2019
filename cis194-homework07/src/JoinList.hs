38e4d0e664b82fb590f81358544fe05c1eb49eac
Wei Ning <macgnw@gmail.com>
2019-09-29 22:03:35 +1000

    wip

--- cis194-homework07/src/JoinList.hs
+++ cis194-homework07/src/JoinList.hs
@@ -1,6 +1,7 @@
 module JoinList
   ( JoinList(..)
   -- , (+++)
+  , tag
   ) where
 
 data JoinList m a = Empty
@@ -8,7 +9,11 @@ data JoinList m a = Empty
                   | Append m (JoinList m a) (JoinList m a)
                   deriving (Eq, Show)
 
--- (+++) :: Monoid m => JoinList m a -> JoinList m a -> JoinList m a
+(+++) :: Monoid m => JoinList m a -> JoinList m a -> JoinList m a
+(+++) a b = a
 
-
+tag :: Monoid m => JoinList m a -> m
+tag (Single m a)       = m
+tag (Append _ lhs rhs) = tag lhs `mappend` tag rhs
+tag _                  = mempty
 
--- cis194-homework07/test/Spec.hs
+++ cis194-homework07/test/Spec.hs
@@ -1,2 +1,20 @@
+
+import           Test.Hspec
+
+import           JoinList
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $
+  describe "JoinList API" $ do
+    it "can create an empty list" $ do
+      let em = Empty
+      tag em `shouldBe` ""
+    it "creates a join list with tag" $ do
+      let a = Single "A" ""
+      tag a `shouldBe` "A"
+    it "creates a join list by appending two child lists" $ do
+      let a = Single "A" ""
+      let b = Single "B" ""
+      let c = Append "C" a b
+      tag c `shouldBe` "AB"
+
